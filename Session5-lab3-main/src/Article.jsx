b211666c57c620ec94538c162205fc6b90532ac2
sahilpreet singh <sahilpreetsingh84884@gmail.com>
2024-10-12 12:04:44 +0300

    update

---
+++ Session5-lab3-main/src/Article.jsx
@@ -0,0 +1,13 @@
+// src/Article.jsx
+import React from 'react';
+
+function Article(props) {
+  return (
+    <article>
+      <h3>{props.title}</h3>
+      <p>{props.text}</p>
+    </article>
+  );
+}
+
+export default Article;
